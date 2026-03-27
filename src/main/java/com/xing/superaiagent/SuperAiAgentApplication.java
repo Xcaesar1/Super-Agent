fb38cf6cb9cb3d37e6d02cfa5dfd884c2c375873
liyuxing <GOD3KILL@163.COM>
2026-03-27 18:00:05 +0800

    Changes

---
+++ src/main/java/com/xing/superaiagent/SuperAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xing.superaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xing/superaiagent/SuperAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xing.superaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
